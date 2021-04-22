776362fbdf38e43342a918ef51a66ef117f82075
absltn000 <78900100+absltn000@users.noreply.github.com>
2021-04-22 15:43:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,14 +2,18 @@
 
 int countPairs1(int *arr, int len, int value) {
   int s = 0;
-  for (int i=0; i < len - 1; i++)
-    for (int j = i + 1; j < len; j++)
-      if (arr[i] + arr[j] == value)
+  for (int i=0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         s++;
+      }
+    }
+  }
   return s;
 }
 
 int countPairs2(int *arr, int len, int value) {
+  int s = 0;
   int i = 0, j = len - 1, s = 0;
   while (i != j)
     if (arr[i] + arr[j] == value) {
@@ -22,6 +26,7 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
+  int s = 0;
   for (int h = 0; h < len - 1; h++)
   {
     int i = h + 1;
